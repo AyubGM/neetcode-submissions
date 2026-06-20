c83defec182a9b0dc18ef398f5b90daa4431cd9d
AyubGM <135363737+AyubGM@users.noreply.github.com>
2026-06-20 12:32:46 +0200

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        nums.push_back(0);
+        nums.push_back(0);
+
+        for (int i = nums.size() - 4; i >= 0; i--) {
+            nums[i] += std::max(nums[i + 2], nums[i + 3]);
+        }
+
+        return std::max(nums[0], nums[1]);
+    }
+};
