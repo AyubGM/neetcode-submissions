8fd4f706e1aac19a2b6e85937e426a23b5cd2226
AyubGM <135363737+AyubGM@users.noreply.github.com>
2026-09-06 22:18:59 +0200

    Add: asteroid-collision - submission-3

---
+++ Data Structures & Algorithms/asteroid-collision/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        std::vector<int> st;
+
+        for (int ast : asteroids)
+        {
+            bool destroyed = false;
+          while (!st.empty() && st.back() > 0 && ast < 0) {
+                if (st.back() < -ast) {
+                    st.pop_back(); 
+                } else if (st.back() == -ast) {
+                    st.pop_back(); 
+                    destroyed = true;
+                    break;
+                } else {
+                    destroyed = true;
+                    break;
+                }
+            }
+
+            if (!destroyed) {
+                st.push_back(ast);
+            }
+
+        }
+
+        return st;
+    }
+};
