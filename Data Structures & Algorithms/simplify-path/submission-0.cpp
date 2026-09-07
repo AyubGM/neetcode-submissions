aa742b1d0d7ae2bf9a5f378694073828fc1f9f78
AyubGM <135363737+AyubGM@users.noreply.github.com>
2026-09-07 04:09:21 +0200

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        std::stringstream ss(path);
+        string token;
+
+        
+        while (getline(ss, token, '/'))
+        {
+            if (token == "" || token == ".")
+            {
+                continue;
+            }
+
+            if (token == "..")
+            {
+                if (!st.empty())
+                {
+                    st.pop_back();
+                }
+            } else {
+                st.push_back(token);
+            }
+        }
+
+        string res = "";
+        for (const string& dir : st)
+        {
+            res += "/" + dir;
+        }
+
+        return res.empty() ? "/" : res; 
+    }
+};
